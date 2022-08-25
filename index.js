196f2baa51ca7600cf76dede779bfcb6981fd6ad
FrannCoppola <104442480+FrannCoppola@users.noreply.github.com>
2022-08-24 21:13:49 -0300

    subida de archivos

---
+++ index.js
@@ -0,0 +1,21 @@
+/* let numeroUno = 10;
+let numeroDos = 23;
+let numeroTres = 5;
+
+let suma = numeroUno + numeroTres;
+
+console.log("resultado", suma);
+ */
+
+/* let saludo = "Buenos dias ";
+let nombre = prompt("ingrese su nombre");
+let bienvenida = (saludo + nombre);
+
+alert (bienvenida); */
+
+for(let i = 1; i <=10; i++){
+    let nombreyapellido = prompt ("Ingrese su nombre y apellido, porfavor");
+    let mensaje = `Turno n°${i} Nombre: ${nombreyapellido}`;
+    alert(mensaje);
+}
+alert("¡No hay mas turnos disponibles, vuelva a intentarlo mañana!");
