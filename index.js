974e983af3ac561568ab5bc044df26fe22c3f9c8
Franco Coppola <fcoppola741@gmail.com>
2022-09-02 10:53:55 -0300

    config de prettier y arreglos

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
 let gastoTotal = 0;
 let contador = 0;
 
-alert(`Sume los productos que desea agregar al carrito// coloque 0 para finalizar.`);
+alert(
+	`Sume los productos que desea agregar al carrito// coloque 0 para finalizar.`
+);
 
 ingresarProductos();
 mostrarTotal();
 
 function ingresarProductos() {
-    const fideos = 100;
-    const agua = 240;
-    const pan = 320;
-    const caramelos = 30;
-    let precioProductos = Number(
-        prompt(
-            `Ingrese el nombre del producto que desea agregar ${
-                contador + 1
-            } : \n\n1- Fideos $${fideos} \n2- Agua $${agua} \n3- Pan $${pan} \n4- Caramelos $${caramelos}`
-        )
-    );
+	const fideos = 100;
+	const agua = 240;
+	const pan = 320;
+	const caramelos = 30;
+	let precioProductos = Number(
+		prompt(
+			`Ingrese el nombre del producto que desea agregar ${
+				contador + 1
+			} : \n\n1- Fideos $${fideos} \n2- Agua $${agua} \n3- Pan $${pan} \n4- Caramelos $${caramelos}`
+		)
+	);
 
-    while (precioProductos !=0) {
-        gastoTotal = calcularTotal(precioProductos);
-        contador++;
-        precioProductos = Number( 
-            prompt(
-                `Ingrese el precio del producto que desea agregar ${
-                    contador + 1
-                } : \n\n1- Fideos $${fideos} \n2- Agua $${agua} \n3- Pan $${pan} \n4- Caramelos $${caramelos}`
-            )
-        );
-    }
+	while (precioProductos != 0) {
+		gastoTotal = calcularTotal(precioProductos);
+		contador++;
+		precioProductos = Number(
+			prompt(
+				`Ingrese el precio del producto que desea agregar ${
+					contador + 1
+				} : \n\n1- Fideos $${fideos} \n2- Agua $${agua} \n3- Pan $${pan} \n4- Caramelos $${caramelos}`
+			)
+		);
+	}
 
-    if ((precioProductos = 0)) {
-        alert(`Ha terminado de agregar productos`)
-        
-    }
-
+	if ((precioProductos = 0)) {
+		alert(`Ha terminado de agregar productos`);
+	}
 }
 
 function calcularTotal(precioProductos) {
-    return gastoTotal + precioProductos;
+	return gastoTotal + precioProductos;
 }
 
 function mostrarTotal() {
-    alert(`Cantidad de productos: ${contador} \n\nEl total es de $${gastoTotal}`);
+	alert(
+		`Cantidad de productos: ${contador} \n\nEl total es de $${gastoTotal}`
+	);
 }
